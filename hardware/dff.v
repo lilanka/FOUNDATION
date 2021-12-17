5f2caa2669f4b795732d5bf65e5670a3fb9fece3
lilanka <pathirage.lilanka@gmail.com>
2021-12-17 20:46:22 +0530

    reg.v logic change. reg.v doesn't takes inputs. fix it

--- hardware/dff.v
+++ hardware/dff.v
@@ -1,11 +1,10 @@
 module Dff(output reg q, qb, input in, clk);
   initial begin
-    q = 1'b0;
-    qb = 1'b1;
+    q=1'b0;
+    qb=1'b1;
   end
-
   always @ (posedge clk) begin
-    q = in;
-    qb = ~q;
+    q=in;
+    qb=~q;
   end
 endmodule
--- hardware/reg_tb.v
+++ hardware/reg_tb.v
@@ -2,19 +2,21 @@
 
 module reg_tb;
   reg [15:0] in;
-  reg clk, lg;
+  reg clk, ld;
   wire [15:0] q;
 
-  Reg16 u1(q, in, clk, ld);
-
+  Reg16 u0(q, in, clk, ld);
+    initial begin
+    in=16'd10;clk=1'b1;ld=1'b1;
+    # 10
+    # 10 
+    in=16'd2001;clk=1'b0;ld=1'b1;
+    # 10 
+    in=16'd2051;clk=1'b1;ld=1'b0;
+    # 10 
+    in=16'd2351;clk=1'b0;ld=1'b0;
+    end
   initial begin
-    $monitor("q=%b, in=%b, clk=%b", q, in, clk);
-      clk=1'b0;
-      in =16'b1101100011010101;
-      #10
-
-      #10
-      clk = 1'b1;
-      in =16'b1101111011011101;
+    $monitor("q=%b, in=%b, clk=%b, ld=%b", q, in, clk, ld);
   end
 endmodule
