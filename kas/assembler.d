ecabbcdf442040b1b812fd42e6807979c05a33d6
lilanka <pathirage.lilanka@gmail.com>
2021-12-28 16:51:25 +0530

    nop support

--- kas/assembler.d
+++ kas/assembler.d
@@ -15,7 +15,7 @@ private ubyte[] output;
 private int pass;
 
 // address of labels
-private ushort addr;
+private ushort addr;	// 65535 
 
 // RISC-V assm instructions
 private string label;
@@ -25,6 +25,13 @@ private string r2;
 private string r3;
 private string cmnt;
 
+struct symtab {
+	string lab;		
+	ushort val;
+}
+
+private symtab[] stab;
+
 private void error(string err) {
 	stderr.writeln("kas: " ~ to!string(line_no + 1) ~ ": " ~ err);
 	enforce(0);
@@ -33,6 +40,51 @@ private void error(string err) {
 private void file_write(string outfile) {
 	import std.file : write;
 	write(outfile, output);
+}
+
+private void addsym() {
+	for (size_t i=0; i < stab.length; i++) {
+		if (label == stab[i].lab)
+			error("KAS: Duplicate label: " ~ label);
+	}	
+	symtab newsym = { label, addr };
+	stab ~= newsym;
+}
+
+private void pass_action(ushort size, int outbyte) {
+	if (pass == 1) {
+		// symbol for label
+		if (!label.empty)
+			addsym();	
+
+		addr += size;
+	} else {
+		if (outbyte >= 0)
+			output ~= cast(ubyte)outbyte;
+	}
+}
+
+private void argcheck(bool arg) {
+	if (arg == false)
+		error("KAS: Not valid argument or 'nop' argument: " ~ op);
+}
+
+private void nop() {
+	argcheck(r1 == "x0" && r2 == "x0" && r3 == "0");
+	pass_action(1, 0x00);
+}
+
+private void process() {
+	// when putting label by itself or totally blank line
+	if (op.empty && r1.empty && r2.empty) 
+		pass_action(0, -1);
+		return;
+
+	if (op == "nop")
+		nop();
+
+	else
+		error("KAS: Unknown instructions: " ~ op);
 }
 
 private void parse(string line) {
@@ -75,30 +127,40 @@ private void parse(string line) {
 
 	// todo: don't code as "label: op". If you do, add supportive module for that 
 	// I'm too lazy :)
-}
 
+	// for debugging
+	writeln(label);
+	writeln(op);
+	writeln(r3);
+	writeln(r2);
+	writeln(r1);
+	writeln(cmnt);
+}
 
 private void assemble(string[] lines, string outfile) {
 	pass = 1;
 	for (line_no = 0; line_no < lines.length; line_no++) {
 		parse(lines[line_no]);
+		process();
 	}
 
 	pass = 2;
 	for (line_no = 0; line_no < lines.length; line_no++) {
 		parse(lines[line_no]);
+		process();
 	}
+
 	file_write(outfile);
 }
 
 void main(string[] args) {
 	if (args.length != 2) {
-		stderr.writeln("kas: Input '.asm' file");
+		stderr.writeln("KAS: Input '.asm' file");
 		return;
 	}
 	string[] lines = splitLines(cast(string)read(args[1]));
 	auto split = args[1].findSplit(".asm");	// assembly code should be written in .asm file format
-	auto outfile = split[0] ~ ".kbin";			// The output binary format
+	auto outfile = split[0] ~ ".kbin";			// The output binary format කැබින්   
 
 	assemble(lines, outfile);
 }
